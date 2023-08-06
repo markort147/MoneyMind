340ca78f5279fc7c71dd3c031f00dda6076b016e
Marco Romano <markort147@gmail.com>
2023-08-06 20:29:54 +0200

    make bat actually working

--- MoneyMind_win.bat
+++ MoneyMind_win.bat
@@ -1,3 +1,25 @@
 @echo off
+
+python --version | findstr /C:"Python 3.11" >nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: please install Python 3.11.
+    echo If it is already installed, insert the bin path into the PATH variable.
+    exit /b 1
+)
+
+IF NOT EXIST venv (
+    echo I'm creating a virual environment just for you...
+    echo It can takes few minutes, please wait
+    echo If this window closes at the end of the process, please reopen it and enjoy!
+    python -m venv venv
+
+    venv\Scripts\activate
+
+    pip install -r requirements.txt
+
+    deactivate
+)
+
 call .\venv\Scripts\activate.bat
 python .\main.py
+deactivate
